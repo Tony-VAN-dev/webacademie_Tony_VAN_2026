42fc77e28b2c4acfcd5623a7f6e5a7bbc62f867a
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2026-03-09 22:28:30 +0100

    Ajout de condition à propos de la sessionId, MAJ du CSS

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function redirectionDashboard(){
-    window.location("dashboard.html");
+    window.location.href = "dashboard.html";
+}
+
+// 
+let deuxiemeCoteNavBar = document.querySelector(".deuxiemeCoteNavBar");
+
+if(localStorage.getItem("sessionId") == null)
+{
+    console.log("Y'a pas d'id de session");
+    // on créé un élément avec comme contenu "Se connecter"
+    let seConnecterElement = document.createElement("p");
+    seConnecterElement.textContent = "Se connecter";
+    seConnecterElement.style.position = "absolute";
+    deuxiemeCoteNavBar.appendChild(seConnecterElement);
 }
 
