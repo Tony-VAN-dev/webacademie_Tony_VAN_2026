245bed4768af61abb500778c10177e7cbb078923
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2026-03-11 15:26:52 +0100

    MAJ JS, création page connexion HTML
    Création de plusieurs fonctions, pour faire une simulation de connexion, ajouts de plusieurs variables

--- script.js
+++ script.js
@@ -4,23 +4,46 @@ function redirectionDashboard(){
 
 // 
 // let navBar = document.querySelector(".navBar");
-let deuxiemeCoteNavBar = document.querySelector(".deuxiemeCoteNavBar");
+// let deuxiemeCoteNavBar = document.querySelector(".deuxiemeCoteNavBar");
 // deuxiemeCoteNavBar.style.postion = "relative";
 
-if(localStorage.getItem("sessionId") == null)
-{
-    console.log("Y'a pas d'id de session");
-    // on créé un élément avec comme contenu "Se connecter"
-    let seConnecterElement = document.createElement("p");
-    seConnecterElement.textContent = "Se connecter";
-    deuxiemeCoteNavBar.appendChild(seConnecterElement);
-    // si la souris est au dessus de l'élément "se connecter"
-    seConnecterElement.addEventListener("click", () => 
-        {
-            let seConnecter = document.createElement("p");
-            seConnecter.textContent = "Se connecter";
-            seConnecter.style.position = "absolute";
-            deuxiemeCoteNavBar.appendChild(seConnecter);
-        });
+
+// if(localStorage.getItem("sessionId") == null)
+// {
+//     console.log("Y'a pas d'id de session");
+//     // on créé un élément avec comme contenu "Se connecter"
+//     let seConnecterElement = document.createElement("p");
+//     seConnecterElement.textContent = "Se connecter";
+//     deuxiemeCoteNavBar.appendChild(seConnecterElement);
+//     // si la souris est au dessus de l'élément "se connecter"
+//     seConnecterElement.addEventListener("click", () => 
+//         {
+//             let seConnecter = document.createElement("p");
+//             seConnecter.textContent = "Se connecter";
+//             seConnecter.style.position = "absolute";
+//             deuxiemeCoteNavBar.appendChild(seConnecter);
+//         });
+// }
+
+// variables
+let adresseEmailInput = document.getElementById("adresseEmail");
+let boutonConnexion = document.getElementById("boutonConnexion");
+
+    boutonConnexion.addEventListener("click", () => 
+    {
+        recupererDonneesConnexion();
+    });
+
+// quand on appuie sur le bouton pour se connecter
+function recupererDonneesConnexion(){
+    adresseEmailInput.value
 }
 
+//vérifier session après click bouton de connexion
+function verifierSessionId()
+{
+    if(localStorage.getItem("sessionId") == null)
+    {
+        //il faut récupérer les données
+    }
+}
