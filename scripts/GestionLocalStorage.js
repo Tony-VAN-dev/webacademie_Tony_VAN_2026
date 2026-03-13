c4746c519a48c6adb7d4e51b30eaf2240d9f389f
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2026-03-13 16:18:00 +0100

    MAJ HTML, JS

--- scripts/GestionLocalStorage.js
+++ scripts/GestionLocalStorage.js
@@ -8,15 +8,18 @@ class gestionLocalStorage{
     creerUnCompte() // fonction principale
         {
             let donnees = recupererDonneesConnexion();
+            this.verificationDesDonnees(donnees);
             
         }
 
     creerCompteLocalStorage(donnees)
     {
         localStorage.setItem("session", "1234");
+        localStorage.setItem("adresseMail", donnees[1]);
+        localStorage.setItem("adresseMail", donnees[2]);
     }
 
-
+    // quand on appuie sur le bouton pour se connecter
     recupererDonneesConnexion()
     {
         donnees = []; // [0] = adresseEmailValeur, [1] motDePasseValeur, [2] motDePasseValeur2
@@ -32,9 +35,22 @@ class gestionLocalStorage{
         return donnees;
     }
     
-    verificationDesDonnees()
+    verificationDesDonnees(donneesConnexion, donneesLocalStorage)
     {
+        // si le mot de passe est pareil que l'autre mot de passe écrit
+        if(donneesConnexion[1] == donnees[2])
+        {
+            console.log("mot de passe correct");
+        }
 
+    }
+
+    verifierSessionId()
+    {
+        if(localStorage.getItem("sessionId") == null)
+        {
+            console.log("Pas de sessionId détecté");
+        }
     }
 
 }
--- scripts/main.js
+++ scripts/main.js
@@ -27,20 +27,7 @@ function redirectionDashboard(){
 
 
 
-// quand on appuie sur le bouton pour se connecter
-function recupererDonneesConnexion(){
-    let donnees = []; // [0] = adresseEmailValeur, [1] motDePasseValeur, [2] motDePasseValeur2
-    let adresseEmailValeur = adresseEmailInput.value;
-    let motDePasseValeur = motDePasseInput.value;
-    let motDePasseValeur2 = motDePasseInput2.value;
-    let donneesPourBoucle = [adresseEmailValeur, motDePasseValeur, motDePasseValeur2];
 
-    for(let i = 0 ; i < 3 ; i++)
-    {
-        donnees.push(donneesPourBoucle[i]);
-    }
-    return donnees;
-}
 // vérification des données écrites dans la page de connexion
 function verificationDesDonnees(donneesConnexion, donneesLocalStorage)
 {
