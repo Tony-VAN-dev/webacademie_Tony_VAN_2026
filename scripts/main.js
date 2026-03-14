2156a0ead928357b96f68babb7cdabb3d20218f0
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2026-03-14 02:32:39 +0100

    MAJ JS et CSS
    J'ai fais en sorte qu'on puisse toggle la classe hidden pour le formulaire à chaque clic sur "Se connecter"

--- scripts/main.js
+++ scripts/main.js
@@ -24,54 +24,65 @@ if(localStorage.getItem("sessionId") == null)
     // si la souris est au dessus de l'élément "se connecter"
     seConnecterElement.addEventListener("click", () => 
         {
-            //formulaire
-            let formulaire = document.createElement("div");
-            formulaire.classList.add("formulaire");
-            // presentationLifePlannerSection.appendChild(formulaire);
-            document.body.appendChild(formulaire);
-
-
-
-            // email
-            let labelEmail = document.createElement("label");
-            labelEmail.textContent = "Entrez votre adresse mail";
-            let inputEmail = document.createElement("input");
-            inputEmail.setAttribute("placeholder","Entrez votre adresse mail");
-            // mot de passe
-            let labelMdp = document.createElement("label");
-            labelMdp.textContent = "Entrez votre mot de passe";
-            let inputMdp = document.createElement("input");
-            inputMdp.setAttribute("type","password");
-            inputMdp.style.display = "block";
-            
-            // mot de passe
-            let labelMdp2 = document.createElement("label");
-            labelMdp2.textContent = "Entrez votre mot de passe";
-            let inputMdp2 = document.createElement("input");
-            inputMdp2.setAttribute("type","password");
-            inputMdp2.style.display = "block";
-
-            // se connecter
-            let seConnecter = document.createElement("button");
-            seConnecter.textContent = "Se connecter";
-            seConnecter.style.position = "absolute";
-            seConnecter.style.textAlign = "left";
-
-            // ajouts d'enfants au formulaire
-            
-            let tableauEnfants_a_ajouter = [
-            labelEmail,
-            inputEmail,
-            labelMdp,
-            inputMdp,
-            inputMdp2,
-            seConnecter
-            ];
-
-            tableauEnfants_a_ajouter.forEach((e) =>
-            {
-                formulaire.appendChild(e);
-                console.log(e);
-            });
+            console.log(formulaire);
+            formulaire.classList.toggle("hidden");
         });
 }
+
+function creerformulaire()
+{
+    //formulaire
+    let formulaire = document.createElement("div");
+    formulaire.classList.add("formulaire");
+    formulaire.classList.add("hidden");
+    // presentationLifePlannerSection.appendChild(formulaire);
+    document.body.appendChild(formulaire);
+
+
+
+    // email
+    let labelEmail = document.createElement("label");
+    labelEmail.textContent = "Entrez votre adresse mail";
+    let inputEmail = document.createElement("input");
+    inputEmail.setAttribute("placeholder","Entrez votre adresse mail");
+    // mot de passe
+    let labelMdp = document.createElement("label");
+    labelMdp.textContent = "Entrez votre mot de passe";
+    let inputMdp = document.createElement("input");
+    inputMdp.setAttribute("type","password");
+    inputMdp.style.display = "block";
+    
+    // mot de passe
+    let labelMdp2 = document.createElement("label");
+    labelMdp2.textContent = "Entrez votre mot de passe";
+    let inputMdp2 = document.createElement("input");
+    inputMdp2.setAttribute("type","password");
+    inputMdp2.style.display = "block";
+
+    // se connecter
+    let seConnecter = document.createElement("button");
+    seConnecter.textContent = "Se connecter";
+    seConnecter.style.position = "absolute";
+    seConnecter.style.textAlign = "left";
+
+    // ajouts d'enfants au formulaire
+    
+    let tableauEnfants_a_ajouter = [
+    labelEmail,
+    inputEmail,
+    labelMdp,
+    inputMdp,
+    inputMdp2,
+    seConnecter
+    ];
+
+    tableauEnfants_a_ajouter.forEach((e) =>
+    {
+        formulaire.appendChild(e);
+        console.log(e);
+    });
+
+    return formulaire;
+}
+
+let formulaire = creerformulaire();
