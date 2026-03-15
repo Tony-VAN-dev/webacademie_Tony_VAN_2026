0ebb1a13ea4419f4e96275e43d92f08ee686bf86
Tony-VAN-dev <TONY.VAN@latoile.em-lyon.com>
2026-03-15 21:50:55 +0100

    MAJ JS et HTML (dashboard)

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,5 +1,43 @@
-let tache = [];
+function chercherUtilisateurParSessionId(tache)
+{
+    let sessionId = localStorage.getItem("sessionId");
+    let utilisateurs = localStorage.getItem("utilisateurs");
+
+    utilisateurs.forEach(e)
+    {
+        if(e.id == sessionId)
+        {
+
+        }
+    }
+}
+//variables dashboard
+let tachesBloc = document.getElementsByClassName("tachesBloc");
 let creerPageTacheBouton = document.getElementById("creerPageTachesBouton");
-creerPageTacheBouton.addEventListener("click", (e)=>{
-    let 
+creerPageTacheBouton.addEventListener("click", (e)=>
+{
+    // div page tache
+    let divPageTache = document.createElement("div");
+    divPageTache.classList.add("blocTache");
+    //créer tache bouton
+    let creerTacheBouton = document.createElement("button");
+    creerPageTacheBouton.textContent = "Créer une tâche";
+    creerTacheBouton.addEventListener("click", (e)=>
+    {
+        // let document.createElement("select");
+        let tacheTexte = document.createElement("p");
+        let boutonModifierTache = document.createElement("button");
+        boutonModifierTache.addEventListener("click", (e)=>
+        {
+            // il faut stocker la tâche dans localStorage
+            
+
+        })
+    });  
+    
 });
+
+function affichageTaches()
+{
+    
+}
